059781b5ab746749047fee84d492edab380cd8c3
Asif <asifhaque0505@gmail.com>
2024-04-03 02:04:35 +0530

    .populate()

--- routes/team.js
+++ routes/team.js
@@ -21,7 +21,8 @@ router.route("/").post(async (req, res) => {
       .status(400)
       .json({ error: true, message: "Maximum members in the team = 11" });
   try {
-    const team = await Teams.create(req.body).populate("members");
+    const team = await Teams.create(req.body);
+    await team.populate("members");
     return res
       .status(201)
       .json({ message: team.name + " created!", data: team });
